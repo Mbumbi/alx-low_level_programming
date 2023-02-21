5b55a9cb66e63efda2d2c88d55f8f32fee88e5b8
Mbumbi <musaumumo1989@gmail.com>
2023-02-21 15:52:54 -0800

    update

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - will write the characterc to student
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errors set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, $c, 1));
+}
